c917d2f1a893fdeacb0e1a5c9c2f03236323757e
Connor Sullivan <connorjs25@outlook.com>
2021-12-01 23:34:22 -0500

    ① Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun count(input: List<Int>, window: Int = 1): Int {
+        assert(input.isNotEmpty())
+        var count = 0
+        var prev = input[0]
+        for (index in window until input.size) {
+            val curr = input[index]
+            if (curr > prev) {
+                ++count
+            }
+            prev = input[index - window + 1]
+        }
+        return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    /** Count the number of times a depth measurement increases from the previous measurement. */
+    fun part1(input: List<Int>): Int {
+        return count(input)
+    }
+
+    /** Count the number of times the sum of measurements in this sliding window (length 3) increases from the previous sum. */
+    fun part2(input: List<Int>): Int {
+        return count(input, 3)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01").map { it.toInt() }
+    check(part1(input) == 1316)
+    check(part2(input) == 1344)
 }
